b13ad64c5a70eb15f960728294e4a6f8641be0a0
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2025-04-09 23:56:49 +0200

    added smart buttons

--- app.js
+++ app.js
@@ -3,36 +3,29 @@ const app = Vue.createApp({
         return {
             name: "Carrell",
             shopping_items: ['mela','pera','banana','caffe'],
-           // timeout: true,
+            timeout: true,
         }
     },
-    mounted() {
-        this.isTimeout();
-    },
     template: `
-                <div v-if="timeout">
+                <div v-if="timeout"  class="flex justify-center items-center h-screen">
                     <img class="animate__animated animate__heartBeat animate__infinite rounded-full" src="assets/logo.png">
                     <span class="piskel">loading...</span>
                 </div>
                 <div v-else class="bg-[#abe54d] flex justify-center items-center h-screen">
                     <ul class="main_table piskel">
                     <div class="title">Shoppinz List</div>
-                    <li v-for='item in shopping_items'>{{item}}</li>
+                    <li v-for='(item,index) in shopping_items' :key='index'>{{item}}
+                        <button v-if='index === shopping_items.length-1'>+</button>
+                        <button v-else='index != shopping_items.length-1'>-</button>
+                    </li>
                     </ul>
                 </div>
     `,
-    methods:{
-        isTimeout() {
-            setTimeout(() => {
-                this.timeout = false;
-            }, 100);
-        }
-    },
     mounted(){
         axios.get('https://ohanahome.altervista.org/public/shopping_list')
             .then(response => {
-                console.log(response);
                 this.shopping_items = response.data.data.map(item =>{ return item.prodotto});
+                this.timeout = false;
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
