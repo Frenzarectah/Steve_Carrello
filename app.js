0d2033e836008cbe36b08b7ffb242bc308cc3585
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2025-04-16 23:32:09 +0200

    added new logo and font, plus added logic to delete item frontend

--- app.js
+++ app.js
@@ -2,21 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             name: "Carrell",
-            shopping_items: ['mela','pera','banana','caffe'],
+            shopping_items: [],
             timeout: true,
         }
     },
     template: `
-                <div v-if="timeout"  class="flex justify-center items-center h-screen">
-                    <img class="animate__animated animate__heartBeat animate__infinite rounded-full" src="assets/logo.png">
-                    <span class="piskel">loading...</span>
+                <div v-if="timeout" class="flex justify-center items-center h-screen bg-[#a4243b]">
+                    <img class="animate__animated animate__heartBeat animate__infinite rounded-full w-1/2" src="assets/logo.png">
                 </div>
-                <div v-else class="bg-[#abe54d] flex justify-center items-center h-screen">
-                    <ul class="main_table piskel">
-                    <div class="title">Shoppinz List</div>
-                    <li v-for='(item,index) in shopping_items' :key='index'>{{item}}
+                <div v-else class="bg-[#a4243b] flex justify-center items-center h-screen">  
+                <ul class="main_table lilita">
+                <div class="title">Shoppinz List</div>  
+                    <li v-for='(item,index) in shopping_items' :key='index'>{{item.item}}
                         <button v-if='index === shopping_items.length-1'>+</button>
-                        <button v-else='index != shopping_items.length-1'>-</button>
+                        <button v-else @click="deleteItem(item.id)">-</button>
                     </li>
                     </ul>
                 </div>
@@ -24,13 +23,26 @@ const app = Vue.createApp({
     mounted(){
         axios.get('https://ohanahome.altervista.org/public/shopping_list')
             .then(response => {
-                this.shopping_items = response.data.data.map(item =>{ return item.prodotto});
+                this.shopping_items = response.data.data.map(item =>{ return {item: item.prodotto, id: item.id}});
                 this.timeout = false;
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
-    }
+    },
+    methods: {
+        deleteItem(id){
+            axios.delete('https://ohanahome.altervista.org/public/shopping_list/'+id)
+            .then(()=>{
+            return axios.get('https://ohanahome.altervista.org/public/shopping_list');
+            })
+            .then(response => {
+                this.shopping_items = response.data.data.map(item => {
+                    return { item: item.prodotto, id: item.id };
+                });
+            })
+        }    
+    },
 });
 
 app.mount('#steve');
